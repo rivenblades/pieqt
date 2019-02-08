df1e8e7e24444aa50c035e63fb4529ea539706ee
kostas <rivenblades@gmail.com>
2019-02-08 14:32:14 +0200

    "import_submodules"

--- pieqt/__init__.py
+++ pieqt/__init__.py
@@ -1,4 +1,9 @@
 from . import pieqt
 from . import App
-import sys
-sys.path.append('.')
+import pkgutil
+
+__all__ = []
+for loader, module_name, is_pkg in  pkgutil.walk_packages(__path__):
+    __all__.append(module_name)
+    _module = loader.find_module(module_name).load_module(module_name)
+    globals()[module_name] = _module
